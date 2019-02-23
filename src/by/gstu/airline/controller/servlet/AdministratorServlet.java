c97fc60b1bf2a8a5327685bc25dc57c0b86d91c8
RenamedUser <p.e.krivohseev@gmail.com>
2019-02-23 10:34:21 +0300

    change log4j log path

--- src/by/gstu/airline/controller/servlet/AdministratorServlet.java
+++ src/by/gstu/airline/controller/servlet/AdministratorServlet.java
@@ -1,8 +1,5 @@
 package by.gstu.airline.controller.servlet;
 
-import by.gstu.airline.service.Service;
-import com.google.gson.Gson;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +13,4 @@ public class AdministratorServlet extends HttpServlet {
         resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
         req.getRequestDispatcher("administrator.html").forward(req, resp);
     }
-
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//        Gson gson = new Gson();
-//        Service service = Service.INSTANCE;
-//        String json = gson.toJson(service.readAllFlight());
-//        resp.setContentType("application/json; charset=UTF-8");
-//        resp.getWriter().write(json);
-//    }
 }
